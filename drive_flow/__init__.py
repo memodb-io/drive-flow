6f6e9b626e6839d251274107d2bfe957f028f5c5
Gus <jianbaiye@outlook.com>
2024-09-12 17:59:56 +0800

    chore: bump to a3

--- drive_flow/__init__.py
+++ drive_flow/__init__.py
@@ -1,7 +1,7 @@
 from .core import EventEngineCls
 from .types import EventInput
 
-__version__ = "0.0.1.alpha2"
+__version__ = "0.0.1.alpha3"
 __author__ = "Jianbai Ye"
 __url__ = "https://github.com/memodb-io/drive-flow"
 
